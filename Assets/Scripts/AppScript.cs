992e01d322ebe2e96ed6ac7e2794edf7a077219e
George Fedoseev <george.fedoseev@me.com>
2018-11-25 14:52:57 +0400

    receive multiple controllers

--- Assets/Scripts/AppScript.cs
+++ Assets/Scripts/AppScript.cs
@@ -9,7 +9,10 @@ using System.Collections.Concurrent;
 public class AppScript : MonoBehaviour
 {
 
-    public Transform controllerModel;
+
+    public Transform controllersContainer;
+
+
     OSCServer _oscServer;
 
     long _latencySum = 0;
@@ -33,64 +36,34 @@ public class AppScript : MonoBehaviour
         _oscServer = new OSCServer(7003);
         _oscServer.PacketReceivedEvent += (s, packet) => {
 
-            
-            
-            // if(packet.Address == "/c1"){
-                // json approach
-                var str = packet.Data[0] as string;
-                var timestamp = (long)packet.Data[1];
+            Debug.Log($"received OSC {packet.Address}");
 
-            //     if(timestamp < _lastTimestampReceived){
-            //         Debug.LogWarning("Receieved earlier timestamp");
-            //         return;
-            //     }
+            Loom.QueueOnMainThread(() => {
+                var controllerTransform = controllersContainer.Find(packet.Address.Replace("/", ""));
 
-            //     if(_firstPacketReceivedTimestamp == -1){
-            //         _firstPacketReceivedTimestamp = timestamp;
-            //     }
-            //     _lastTimestampReceived = timestamp;
+                if(controllerTransform != null){
+                    // json approach
+                    var str = packet.Data[0] as string;
+                    var timestamp = (long)packet.Data[1];
 
-            //     var latencyMs = System.DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
-            //                     - timestamp;
-            //     _latencySum += latencyMs;
-            //     _framesCount++;
-
-            //     var deltaSeconds = (System.DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _firstPacketReceivedTimestamp)/1000f;
-
-            //     Debug.Log($"Network FPS: {(float)_framesCount/deltaSeconds}");
-
-            //    Debug.Log($"OSC network latencyMs: {latencyMs}, avg: {(float)_latencySum/_framesCount}");
-
-                // var stopwatch = new System.Diagnostics.Stopwatch();
-                // stopwatch.Start();
-                var trackingData = JsonConvert.DeserializeObject<GearVRControllerTrackingData>(str);
-                // Debug.Log($"Time to parse json data: {stopwatch.Elapsed.TotalMilliseconds} ms"); // ~0.1ms
-
-                
-
-                _lastAvailableQuaternion = new Quaternion(
-                    trackingData.quaternion[0],
-                    trackingData.quaternion[1],
-                    trackingData.quaternion[2],
-                    trackingData.quaternion[3]
-                );
-
-
-                //_lastAvailableQuaternion = Quaternion.Inverse(_lastAvailableQuaternion);
-                
-
-                // Loom.QueueOnMainThread(() => {
+                    var trackingData = JsonConvert.DeserializeObject<GearVRControllerTrackingData>(str);
                     
-                // });
-            // }           
-            
+
+                    _lastAvailableQuaternion = new Quaternion(
+                        trackingData.quaternion[0],
+                        trackingData.quaternion[1],
+                        trackingData.quaternion[2],
+                        trackingData.quaternion[3]
+                    );                    
+                    
+                    controllerTransform.localRotation = _zeroQuaternion * _lastAvailableQuaternion;                
+                }
+            });
         };
 
         _oscServer.Connect();
     }
 
-
-    
 
     // Update is called once per frame
     void Update()
@@ -100,8 +73,6 @@ public class AppScript : MonoBehaviour
            Debug.Log("Reset");
            _zeroQuaternion = Quaternion.Inverse(_lastAvailableQuaternion);
        }
-
-       controllerModel.localRotation = _zeroQuaternion * _lastAvailableQuaternion;
             
     }
 
